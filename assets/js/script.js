5a9d332106903d0b86ffefa1b1e12345a23bdacf
LonelyMitoc <jamesmatsu@gmail.com>
2022-09-08 04:58:09 -0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var scoreEl = document.getElementById('#scores');
+var timerEl = document.getElementById('#countdown');
+var quizContainer = document.getElementById('#quiz-container');
+var questionHeading = document.getElementById('#heading-questions');
+var multipleChoice = document.getElementById('#content-body');
+var startQuiz = document.getElementById('#start');
+var showAnswer = document.getElementById('#answer');
+
+
+function countdown() {
+    var timeLeft = 75;
+
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft;   
+            timeLeft--;         
+        } else {
+
+        }
+    })
+}
