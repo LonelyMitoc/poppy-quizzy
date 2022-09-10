6de6c503d5e67ec75d1c5a68ca132613662c012f
LonelyMitoc <jamesmatsu@gmail.com>
2022-09-10 16:25:33 -0700

    added question.js

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        title: 'Which of the following are not commonly used data types?',
+        choices: [
+            'strings',
+            'booleans',
+            'numbers',
+            'alerts',
+        ]
+        , answer: 'alerts',
+    }, 
+    {
+        title: 'What is the type of Pop up boxes available in JavaScript?',
+        choices: [
+            'Alert',
+            'Confirm',
+            'Prompt',
+            'All of the above',
+        ]
+        , answer: 'All of the above',
+    },
+    {
+        title: 'What is a tool that is good to use for debugging and development for content printing?',
+        choices: [
+            'JavaScript',
+            'Terminal/bash',
+            'console.log',
+            'for loops',
+        ]
+        , answer: 'console.log',
+    },
+    {
+        title: 'Which of the following operators can be used in JavaScript?',
+        choices: [
+            '"And" Operator &&',
+            '"or" Operator ||',
+            '"Not" Operator !',
+            'All of the above',
+        ],
+        answer: 'All of the above'
+    },
+    {
+        title: 'Fill in the blank: String values must be enclosed within ______ when being assigned to variables. ',
+            choices:[
+                'quotes',
+                'curly brackets',
+                'commas',
+                'parentheses'
+            ]
+            ,answer: 'quotes'
+            },
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 // Start button -> starts timer and quiz
-// Multiple choices as buttons
+// Multiple choices as buttons, button click -> Next question and answer to question before
+// -if incorrect, subtract time
+// Answer all Qs or timer is 0 -> "Game Over"
+// Game over -> show score and prompt input of initials
+// submit -> store in local storage and display high score, return & clear high score button
 
+// DOM elements
 var scoreEl = document.getElementById('#scores');
 var timerEl = document.getElementById('#countdown');
 var quizContainer = document.getElementById('#quiz-container');
 var questionHeading = document.getElementById('#heading-questions');
 var multipleChoice = document.getElementById('#content-body');
-var startQuiz = document.getElementById('#start');
+var startQuizBtn = document.getElementById('#start');
 var showAnswer = document.getElementById('#answer');
 
+// Quiz state variables
 var timeLeft = 75;
 var highScore = [];
 var userInitials = '';
